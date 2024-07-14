774c19cb8d2e5b79ee8ce60936c3b20089d8a9ef
micheledimassimo <mcldms@outlook.it>
2024-07-14 20:30:23 +0200

    v1.2 restituzione prezzo in pagina

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,20 @@ console.log('Prezzo per km in euro:', ppKM);
 
 const infoForm = document.querySelector('form');
 infoForm.addEventListener(
-    'submit',          // 1° argomento: evento da ascoltare/intercettare
-    function (event) { // 2° argomento: definizione di una funzione anonima che verrà eseguita solo quando si verificherà l'evento sull'elemento 
-        /* 
-            Blocco di codice da eseguire quando si verifica l'evento selezionato
-        */
-       
+    'submit',          
+    function (event) { 
         console.log('event', event, typeof event);
 
         event.preventDefault();
 
         const trip = document.getElementById('trip');
-
         console.log('trip.value', trip.value, typeof trip.value);
-
         let tripInput = parseInt(trip.value);
-        const age = document.getElementById('age');
         
+        const age = document.getElementById('age');
         console.log('age.value', age.value, typeof age.value);
         const ageInput = parseInt(age.value);
+        
         console.log('ageInput.value', ageInput, typeof ageInput);
 
         let price = 0.21 * tripInput;
@@ -34,14 +29,18 @@ infoForm.addEventListener(
         if (ageInput < 18) {
             price = (price - (price * 0.2)).toFixed(2);
             console.log('Prezzo con 20% sconto: €', price); 
+            document.getElementById('ticket-price').innerHTML = price;
         }
         if (ageInput >= 18 && ageInput < 65) {
             price = price.toFixed(2);
             console.log('Prezzo: €', price);
+            document.getElementById('ticket-price').innerHTML = price;
         }
         if (ageInput >= 65) {
             price = (price - (price * 0.4)).toFixed(2);
             console.log('Prezzo con 40% sconto: €', price);
+            document.getElementById('ticket-price').innerHTML = price;
         }
+
     }
 );
